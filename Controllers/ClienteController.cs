37fd47e037dbcc79d96a2e2653486b2d4923951a
Luiz Gonzaga <luiz.neto@rkcomexoffice.onmicrosoft.com>
2020-11-17 12:59:13 -0300

    Criação dos métodos da API de cliente

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -22,15 +22,45 @@ namespace localizaBaba.Web.Controllers
         [HttpGet]
         public ActionResult<List<Cliente>> Get()
         {
-            return _context.Clientes.ToList();
+            return Ok(_context.Clientes.ToList());
         }
 
         [HttpGet]
         public ActionResult<Cliente> Get(int id)
         {
-            return _context.Clientes.FirstOrDefault(c => c.Id == id);
+            var cliente = _context.Clientes.FirstOrDefault(c => c.Id == id);
+            return Ok(cliente);
         }
 
+        [HttpPost]
+        public ActionResult<Cliente> Post(Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            _context.SaveChanges();
+            return Ok(cliente);
+        }
+
+        [HttpPut]
+        public ActionResult<Cliente> Put(Cliente cliente)
+        {
+            var clienteId = _context.Clientes.FirstOrDefault(c => c.Id == cliente.Id);
+            clienteId.Nome = cliente.Nome;
+            clienteId.SobreNome = cliente.SobreNome;
+            clienteId.Senha = cliente.Senha;
+            clienteId.Email = cliente.Email;
+            _context.SaveChanges();
+            return Ok(cliente);
+        }
+
+        [HttpDelete]
+        public ActionResult<Cliente> Delete(int id)
+        {
+            var clienteId = _context.Clientes.FirstOrDefault(c => c.Id == id);
+            _context.Remove(clienteId);
+            _context.SaveChanges();
+            return Ok(clienteId);
+
+        }
 
 
     }
